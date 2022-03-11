5200e4b29419c57ee254fc29bf3dddaf7ab0dfd0
b-beijer <50325630+b-beijer@users.noreply.github.com>
2022-03-11 08:10:41 +0100

    Update jp.rb

--- Formula/jp.rb
+++ Formula/jp.rb
@@ -1,7 +1,7 @@
 class Jp < Formula
   desc "Command-line interface to JMESPath, a query language for JSON"
   homepage "http://jmespath.org/"
-  url "https://github.com/jmespath/jp/releases/download/0.2.0/jp-0.2.0.tar.gz"
+  url "https://github.com/jmespath/jp/releases/download/0.2.1/jp-0.2.1.tar.gz"
   sha256 "8083f87df1bd550f0cddbb143be82f10e2e6cadaf6b633d133656c593f25e666"
   license "Apache-2.0"
 
